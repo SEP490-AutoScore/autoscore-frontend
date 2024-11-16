6091eef99b44b6ccb49e75ad1cd73612f847b019
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2024-11-16 18:42:31 +0700

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import './App.css'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { Routes, Route } from "react-router-dom";
+import "./App.css";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import  Page from "./app/dashboard/page";
+// import { AppSidebar } from "@/components/app-sidebar";
+// import { SidebarProvider } from "@/components/ui/sidebar";
+// import { Routes, Route } from "react-router-dom";
 
-function RoutesWrapper() {
-  const routes = useRoutes(); // Now this is within RouteProvider
+// function RoutesWrapper() {
+//   const routes = useRoutes(); // Now this is within RouteProvider
 
-  return (
-    <Routes>
-    </Routes>
-  );
+//   return (
+//     <Routes>
+//     </Routes>
+//   );
 
-}
+// }
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        {/* Main content goes here */}
-        <h1>Welcome to the Application</h1>
-      </main>
-      <Footer />
+          <Page />
     </div>
   );
 }
-export default App
+export default App;
