d7d2e0ae66b85103e6ad05c74686a726a831875d
VanMinhtp <vanminh101202@gmail.com>
2024-11-17 13:11:20 +0700

    route Update

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "@/app/dashboard/page";
 import Login from "@/app/login/page";
 import ProtectedRoute from "./ProtectedRoute";
+import ScoresOverview from "@/app/score/overview";
+import ScoresPage from "@/app/score/page";
 
 const AppRoutes = () => {
   return (
@@ -12,6 +14,22 @@ const AppRoutes = () => {
         element={
           <ProtectedRoute>
             <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/scores-overview"
+        element={
+          <ProtectedRoute>
+            <ScoresOverview />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/scores"
+        element={
+          <ProtectedRoute>
+            <ScoresPage />
           </ProtectedRoute>
         }
       />
