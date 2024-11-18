89f7aba8638d542a1590cf9651a8938ebe5c7b8a
votrongvuong672 <votrongvuong001@gmail.com>
2024-11-18 15:49:06 +0700

    add some api

--- src/config/apiConfig.tsx
+++ src/config/apiConfig.tsx
@@ -6,4 +6,22 @@ export const API_ENDPOINTS = {
   oauthGoogle: "/oauth2/authorization/google", // Đăng nhập Google OAuth
   signInGoogle: "/api/auth/signingoogle",      // API lấy thông tin JWT
   refreshToken: "/api/auth/refresh-token",     // API refresh token
+
+  // Exam Database 
+  importDatabase: "/api/database/import",
+  changeDatabase: "/api/database/update",
+
+  // Exam Question
+  getlistIdQuestion: "/api/exam-paper//questions",
+
+  // Gherkin
+  generateGherkin: "/api/gherkin_scenario/generate_gherkin_format",
+  getlistIdGherkin: "/api/exam-paper//gherkin-scenarios",
+
+  // Postman
+  importPostman: "/api/exam-paper/import-postman-collections",
+  exportPostman: "/api/exam-paper/export-postman/",
+  generatePostman: "/api/postman-grading/generate/",
+  mergePostman: "/api/postman-grading/merge/"
+
 };
