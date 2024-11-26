6cd3c8d8cf7ed7c71a8cee878e67a0fedc4e56ff
votrongvuong672 <votrongvuong001@gmail.com>
2024-11-26 15:42:59 +0700

    more validate

--- src/app/exam/gherkin-postman/gherkin-postman-layout.tsx
+++ src/app/exam/gherkin-postman/gherkin-postman-layout.tsx
@@ -15,26 +15,16 @@ const GherkinPostmanLayout: React.FC<LayoutProps> = ({ top, middle, left, right 
         <div className="flex">
           {/* Phần bên trái (Gherkin Tool) */}
           <div className="w-1/2 p-4">
-            {top.left}
-          </div>
-
-          <div className="border-l border-gray-300" /> {/* Thanh chia giữa */}
-          {/* Phần bên phải (Postman Tool) */}
-
-          <div className="w-1/2 p-4">
-            {top.right}
+            {top}
           </div>
 
         </div>
-
-        {/* Thanh ngang dưới phần đầu */}
-        {/* <div className="border-b border-gray-300 mt-4" /> */}
+      
       </header>
 
 
       {middle}
-
-
+      
       {/* Phần giữa (Chia đôi màn hình: trái phải) và có thanh chia giữa */}
       <div className="flex flex-1 mb-2">
         {/* Bên trái: Nội dung Gherkin */}
