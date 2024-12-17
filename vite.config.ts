8f7018c65629d1093ed4afd7389bc9addde7c85f
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2024-12-17 20:59:25 +0700

    add config to server

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    host: '0.0.0.0', 
+    port: 5173,
+  },
 })
